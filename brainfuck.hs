0789e909bbb86658718a654adfa6ab017ea04d9a
Hayden <asianhaydenxd@gmail.com>
2022-05-15 14:55:55 -0400

    Add tape and wrap in auxiliary

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,5 +1,9 @@
 module Brainfuck where
 
+data Tape = Tape [Int] Int
+
 interpret :: String -> IO ()
-interpret []     = putStrLn "exit"
-interpret (x:xs) = undefined
+interpret code = interpret' code (Tape [0] 0) where
+    interpret' :: String -> Tape -> IO ()
+    interpret' [] _              = putStrLn "exit"
+    interpret' (c:cs) (Tape t p) = undefined
