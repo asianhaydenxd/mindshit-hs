bca4fcbaf5de8aed80c2d607515d577b36df8709
Hayden <59178038+asianhaydenxd@users.noreply.github.com>
2022-05-16 17:01:14 +0000

    Rename nodes and tokens

--- brainfuck.hs
+++ brainfuck.hs
@@ -22,40 +22,40 @@ setTape :: Int -> Tape -> Tape
 setTape n (Tape t p) = alterTape n $ alterTape (-(t !! p)) (Tape t p)
 
 
-data Token = OpRight | OpLeft | OpPlus | OpMinus | OpRead | OpWrite | OpWhile | OpEnd deriving Eq
+data Token = RightToken | LeftToken | PlusToken | MinusToken | ReadToken | WriteToken | WhileToken | EndToken deriving Eq
 
 tokenize :: String -> [Token]
 tokenize []       = []
-tokenize ('>':xs) = OpRight : tokenize xs
-tokenize ('<':xs) = OpLeft  : tokenize xs
-tokenize ('+':xs) = OpPlus  : tokenize xs
-tokenize ('-':xs) = OpMinus : tokenize xs
-tokenize (',':xs) = OpRead  : tokenize xs
-tokenize ('.':xs) = OpWrite : tokenize xs
-tokenize ('[':xs) = OpWhile : tokenize xs
-tokenize (']':xs) = OpEnd   : tokenize xs
+tokenize ('>':xs) = RightToken : tokenize xs
+tokenize ('<':xs) = LeftToken  : tokenize xs
+tokenize ('+':xs) = PlusToken  : tokenize xs
+tokenize ('-':xs) = MinusToken : tokenize xs
+tokenize (',':xs) = ReadToken  : tokenize xs
+tokenize ('.':xs) = WriteToken : tokenize xs
+tokenize ('[':xs) = WhileToken : tokenize xs
+tokenize (']':xs) = EndToken   : tokenize xs
 tokenize (_:xs)   = tokenize xs
 
 
-data Node = Shift Int | Add Int | Read | Write | Loop [Node]
+data Node = ShiftNode Int | AddNode Int | ReadNode | WriteNode | LoopNode [Node]
 
 parse :: [Token] -> [Node]
 parse [] = []
-parse (OpRead:ts)  = Read  : parse ts
-parse (OpWrite:ts) = Write : parse ts
-parse (OpWhile:ts) = Loop (parse is) : parse os where
+parse (ReadToken:ts)  = ReadNode  : parse ts
+parse (WriteToken:ts) = WriteNode : parse ts
+parse (WhileToken:ts) = LoopNode (parse is) : parse os where
     splitInnerOuter :: [Token] -> ([Token], [Token])
     splitInnerOuter xs = split' xs 0 0 where
-        split' (OpEnd:ys)   i 0 = splitAt i xs
-        split' (OpEnd:ys)   i n = split' ys (i + 1) (n - 1)
-        split' (OpWhile:ys) i n = split' ys (i + 1) (n + 1)
+        split' (EndToken:ys)   i 0 = splitAt i xs
+        split' (EndToken:ys)   i n = split' ys (i + 1) (n - 1)
+        split' (WhileToken:ys) i n = split' ys (i + 1) (n + 1)
         split' (_:ys)       i n = split' ys (i + 1) n
     
     (is, os) = splitInnerOuter ts
 
 parse (t:ts)
-    | t `elem` [OpRight, OpLeft] = let s = sequence [OpRight, OpLeft] (t:ts) in (Shift $ occurDifference OpRight OpLeft s) : parse (drop (length s) (t:ts))
-    | t `elem` [OpPlus, OpMinus] = let s = sequence [OpPlus, OpMinus] (t:ts) in (Add   $ occurDifference OpPlus OpMinus s) : parse (drop (length s) (t:ts))
+    | t `elem` [RightToken, LeftToken] = let s = sequence [RightToken, LeftToken] (t:ts) in (ShiftNode $ occurDifference RightToken LeftToken s) : parse (drop (length s) (t:ts))
+    | t `elem` [PlusToken, MinusToken] = let s = sequence [PlusToken, MinusToken] (t:ts) in (AddNode   $ occurDifference PlusToken MinusToken s) : parse (drop (length s) (t:ts))
     | otherwise                  = parse ts
     where
         sequence :: Eq a => [a] -> [a] -> [a]
@@ -81,10 +81,10 @@ asciiToInt = fromEnum
 interpret :: String -> IO (Tape)
 interpret code = interpret' (parse $ tokenize code) (Tape [0] 0) where
     interpret' :: [Node] -> Tape -> IO (Tape)
-    interpret' _ TapeError    = putStrLn "exit: tapeError" >> return TapeError
-    interpret' [] t           = return t
-    interpret' (Shift n:cs) t = interpret' cs (shiftTape n t)
-    interpret' (Add   n:cs) t = interpret' cs (alterTape n t)
-    interpret' (Read   :cs) t = getChar >>= (\a -> interpret' cs $ setTape (asciiToInt a) t)
-    interpret' (Write  :cs) (Tape t p) = (putChar . intToAscii) (t !! p) >> interpret' cs (Tape t p)
-    interpret' (Loop  c:cs) (Tape t p) = if t !! p > 0 then interpret' c (Tape t p) >>= \tape -> interpret' (Loop c:cs) tape else interpret' cs (Tape t p)
+    interpret' _ TapeError        = putStrLn "exit: tapeError" >> return TapeError
+    interpret' [] t               = return t
+    interpret' (ShiftNode n:cs) t = interpret' cs (shiftTape n t)
+    interpret' (AddNode   n:cs) t = interpret' cs (alterTape n t)
+    interpret' (ReadNode   :cs) t = getChar >>= (\a -> interpret' cs $ setTape (asciiToInt a) t)
+    interpret' (WriteNode  :cs) (Tape t p) = (putChar . intToAscii) (t !! p) >> interpret' cs (Tape t p)
+    interpret' (LoopNode  c:cs) (Tape t p) = if t !! p > 0 then interpret' c (Tape t p) >>= \tape -> interpret' (LoopNode c:cs) tape else interpret' cs (Tape t p)
